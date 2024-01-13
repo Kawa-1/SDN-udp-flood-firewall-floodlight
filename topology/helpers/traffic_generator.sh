2416b8d0556b8a6e10a1463501b8305281e8dfb6
Kamil Sobolak <KamilSobolak@protonmail.com>
2024-01-13 16:48:11 +0100

    traffic generator and topolgy update

--- topology/helpers/traffic_generator.sh
+++ topology/helpers/traffic_generator.sh
@@ -20,3 +20,14 @@ elif [[ "$arg1" == *h4* ]]; then
 elif [[ "$arg1" == *h5* ]]; then
     interface="h5-eth0"
 fi
+
+counter=1
+
+while [ "$counter" -le "$arg3" ]; do
+    packit_command="sudo packit -i $interface -t UDP -d 10.0.0.5 -c $arg2 -w 0"
+    echo "Running command: $packit_command"
+    # Uncomment the line below to execute the packit command
+    $packit_command
+    sleep 0.1
+    ((counter++))
+done
